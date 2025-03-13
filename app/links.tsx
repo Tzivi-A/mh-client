1b124414b29ed110ee83777554c2f30fb57b344f
ShoshanaTzi <shoshitzi@gmail.com>
2025-03-13 13:52:59 +0200

    fix

--- app/links.tsx
+++ app/links.tsx
@@ -1,4 +1,4 @@
-import type { Route } from '../+types/root';
+import type { Route } from './+types/root';
 
 export const links: Route.LinksFunction = () => [
   { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
