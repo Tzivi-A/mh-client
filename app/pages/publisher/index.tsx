c0468aa0b0abc5d17fa2ca42e357dfb811dfdaf2
Shoshana Tzi <shoshana_t@malam.com>
2025-03-25 10:10:13 +0000

    Revert "use tanstack form"

--- app/pages/publisher/index.tsx
+++ app/pages/publisher/index.tsx
@@ -2,51 +2,34 @@ import { Button } from '~/components/button/button';
 import { Card } from '~/components/card/card';
 import { Image } from '~/components/image/image';
 import logo from '~/assets/images/LogoMevaker.png';
-import { AppFormSelect as Select } from '~/components/input/select/app-form-select';
+import Select from '~/components/input/select/select';
 import { useQuery } from '~/api/use-query';
 import { CitiesOptions } from '~/api/mock/select-option';
-import useAppForm from '~/hooks/use-app-form';
+import { useState } from 'react';
 
 export const PublisherPage = () => {
   const query = useQuery('https://jsonplaceholder.typicode.com/todos/1');
-  //const [value, setValue] = useState('');
-  const form = useAppForm({
-    defaultValues: {
-      cities: 'ביתר'
-    },
-    // validators: {
-    //   onChange: ({ value }) =>
-    //     value.firstName === value.lastName && 'FirstName and Last Name may not be the same'
-    // },
-    onSubmit: ({ value }) => {
-      alert(JSON.stringify(value));
-    }
-  });
+  const [value, setValue] = useState('');
+
   return (
-    <main>
-      <form
-        className="form"
-        onSubmit={e => {
-          e.preventDefault();
-          void form.handleSubmit();
-        }}
-      >
+    <div>
+      <Card>
+        <div>Publisher {query?.isPending.toString()}</div>
         <div>
-          <Card>
-            <div>Publisher {query?.isPending.toString()}</div>
-            <div>
-              <Button onClick={() => window.alert('Hello! I am the Mevaker!')} type="submit">
-                Click the Mevaker
-              </Button>
-            </div>
-            <Image src={logo} alt="mevaker" />
-            <form.Field name="cities">
-              {() => <Select options={CitiesOptions} label="ערים" />}
-            </form.Field>
-          </Card>
+          <Button onClick={() => window.alert('Hello! I am the Mevaker!')} type="submit">
+            Click the Mevaker
+          </Button>
         </div>
-      </form>
-    </main>
+        <Image src={logo} alt="mevaker" />
+        <Select
+          options={CitiesOptions}
+          onChange={setValue}
+          label="ערים"
+          id="cities"
+          value={value}
+        />
+      </Card>
+    </div>
   );
 };
 
