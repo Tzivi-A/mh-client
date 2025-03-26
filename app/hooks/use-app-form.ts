23da43577128a175041c49fc8ff06bd903e17274
ShoshanaTzi <shoshitzi@gmail.com>
2025-03-26 11:52:21 +0200

    add to hooks

--- app/hooks/use-app-form.ts
+++ app/hooks/use-app-form.ts
@@ -2,7 +2,7 @@
 import { createFormHook } from '@tanstack/react-form';
 import { fieldContext, formContext } from './form-context';
 import { AppFormSelect } from '~/components/input/select/app-form-select';
-
+//fsdfdsf
 // const AppFormSelect = lazy(() =>
 //   import('~/components/input/select/app-form-select').then(module => ({
 //     default: module.AppFormSelect
