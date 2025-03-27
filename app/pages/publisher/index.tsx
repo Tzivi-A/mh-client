19859d3d10120a961bfd7289a856e8bb02992e7e
ShoshanaTzi <shoshitzi@gmail.com>
2025-03-27 09:29:05 +0200

    fix tsconfig.json

--- app/pages/publisher/index.tsx
+++ app/pages/publisher/index.tsx
@@ -31,7 +31,7 @@ export const PublisherPage = () => {
         className="form"
         onSubmit={e => {
           e.preventDefault();
-          void form.handleSubmit();
+          form.handleSubmit();
         }}
       >
         <div>
