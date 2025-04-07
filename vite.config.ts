6b7c894257bc4599b93a0312cf32a437a9a2ce4c
chaya_b <chaya_b@malam.com>
2025-04-07 11:49:46 +0300

    dev:mock

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,7 @@
 import { reactRouter } from '@react-router/dev/vite';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import path from 'path';
 
-export default defineConfig(({ mode }) => {
-  const envDir = path.resolve(__dirname);
-  const env = loadEnv(mode, envDir, '');
-
-  return {
-    plugins: [reactRouter(), tsconfigPaths()],
-    server: {
-      port: 5173,
-      proxy: {
-        '/api': {
-          target: env.VITE_API_URL,
-          changeOrigin: true,
-          rewrite: path => path.replace(/^\/api/, '')
-        }
-      }
-    }
-  };
+export default defineConfig({
+  plugins: [reactRouter(), tsconfigPaths()]
 });
