4c105337b66435c49b17322652a7c67d327835f2
chaya_b <chaya_b@malam.com>
2025-04-08 17:19:15 +0300

    PublishResult

--- app/pages/registration/index.tsx
+++ app/pages/registration/index.tsx
@@ -1,85 +1,198 @@
 import { Card } from '~/components/card/card';
 import { Table, type TableProps } from '../../components/table/table';
-import { Tag } from 'antd';
-
-export interface DataType {
-  key: string;
-  name: string;
-  age: number;
-  address: string;
-  tags: string[];
-  description: string;
-  dateTime: string;
-  date: string;
-  time: string;
-  status: string;
-}
+import type { PublishResult } from '~/types/publish-result';
+import { FundingTypeEnum } from '~/enums/funding-type';
+import type { ColumnsType } from 'antd/es/table';
+import { sorterString, sorterStringDate } from '~/utils/utils';
 
 export const RegistrationPage = () => {
-  const data: DataType[] = Array.from({ length: 50 }, (_, index) => ({
-    key: (index + 1).toString(),
-    name: `User ${index + 1}`,
-    age: 20 + (index % 30),
-    address: `${index + 1} Example Street`,
-    tags: 20 + (index % 30) > 30 ? ['active'] : ['inactive'],
-    description: `A description for User ${index + 1}`,
-    dateTime: '',
-    date: `2023-01-${String((index % 31) + 1).padStart(2, '0')}`,
-    time: `${(index % 12) + 1}:00 ${index % 2 === 0 ? 'AM' : 'PM'}`,
-    status: 20 + (index % 30) > 30 ? 'active' : 'inactive'
-  }));
+  const data: PublishResult[] = [
+    {
+      ID: '1',
+      FundingType: { ID: FundingTypeEnum.Donation, Name: 'תרומה' },
+      Faction: 'יש עתיד',
+      ElectionCity: 'ביתר',
+      ElectionDate: '10/2/2025',
+      FullName: 'אלון פוטרמן',
+      Country: 'אנגליה',
+      EntityCity: 'ישוב אנגלי',
+      FundingDate: '26/12/2022',
+      FundingAmount: 1000.0
+    },
+    {
+      ID: '2',
+      FundingType: { ID: FundingTypeEnum.Loan, Name: 'הלוואה' },
+      Faction: 'המאוחדת',
+      ElectionCity: 'מודיעין עילית',
+      ElectionDate: '18/1/2025',
+      FullName: 'אפרים כץ',
+      Country: 'ישראל',
+      EntityCity: 'בן שמן (מושב)',
+      FundingDate: '1/3/2025',
+      FundingAmount: 1000.2,
+      LoanBalance: 100000.0
+    },
+    {
+      ID: '3',
+      FundingType: { ID: FundingTypeEnum.Guarantee, Name: 'ערבות' },
+      Faction: 'הכי הכי',
+      ElectionCity: 'ירושלים',
+      ElectionDate: '23/10/2024',
+      FullName: 'בנימין דוד',
+      Country: 'ישראל',
+      EntityCity: 'אופקים',
+      FundingDate: '12/03/2023',
+      FundingAmount: 234.0
+    },
+    {
+      ID: '4',
+      FundingType: { ID: FundingTypeEnum.Loan, Name: 'הלוואה' },
+      Faction: 'הסיעה שלנו',
+      ElectionCity: 'בני ברק',
+      ElectionDate: '27/7/2020',
+      FullName: 'משה ברקת',
+      Country: 'ישראל',
+      EntityCity: 'נתניה',
+      FundingDate: '23/02/2025',
+      FundingAmount: 18.0,
+      LoanBalance: 18.0
+    }
+  ];
 
-  const tableProps: TableProps<DataType> = {
-    data,
-    columns: Object.keys(data[0] || {})
-      .filter(key => !['key', 'date', 'time'].includes(key))
-      .map((key, index) => ({
-        title: key.charAt(0).toUpperCase() + key.slice(1),
-        dataIndex: key,
-        key: (index + 1).toString(),
-        align: 'center',
-        render: (_, record) => {
-          if (key === 'tags') {
-            return (
-              <>
-                {record.tags.map(tag => {
-                  const color = tag === 'inactive' ? 'volcano' : 'green';
-                  return (
-                    <Tag color={color} key={tag}>
-                      {tag.toUpperCase()}
-                    </Tag>
-                  );
-                })}
-              </>
-            );
-          }
-          return <span>{record[key]}</span>;
-        },
-        children:
-          key === 'dateTime'
-            ? [
-                {
-                  title: 'תאריך',
-                  dataIndex: 'date',
-                  key: ((index + 1) * data.length).toString(),
-                  align: 'center'
-                },
-                {
-                  title: 'שעה',
-                  dataIndex: 'time',
-                  key: ((index + 1) * data.length + 1).toString(),
-                  align: 'center'
-                }
-              ]
-            : undefined
+  const columns: ColumnsType<PublishResult> = [
+    {
+      title: 'סוג מימון',
+      dataIndex: 'FundingType',
+      key: 'fundingType',
+      filters: Array.from(new Set(data.map(d => d.FundingType.Name))).map(name => ({
+        text: name,
+        value: name
       })),
+      onFilter: (value, record) => record.FundingType.Name === value,
+      sorter: (a, b) => sorterString(a.FundingType.Name, b.FundingType.Name),
+      render: fundingType => {
+        const icons = {
+          [FundingTypeEnum.Donation]: 1,
+          [FundingTypeEnum.Loan]: 2,
+          [FundingTypeEnum.Guarantee]: 3
+        };
+        const icon = icons[fundingType.ID] || -1;
+
+        return (
+          <span>
+            icon: {icon} {fundingType.Name}
+          </span>
+        );
+      },
+      align: 'right'
+    },
+    {
+      title: 'מאפייני בחירות',
+      key: 'electionCharacteristics',
+      align: 'center',
+      children: [
+        {
+          title: 'סיעה',
+          dataIndex: 'Faction',
+          key: 'faction',
+          align: 'right',
+          sorter: (a, b) => sorterString(a.Faction, b.Faction)
+        },
+        {
+          title: 'ישוב',
+          dataIndex: 'ElectionCity',
+          key: 'electionCity',
+          align: 'right',
+          sorter: (a, b) => sorterString(a.ElectionCity, b.ElectionCity)
+        },
+        {
+          title: 'תאריך בחירות',
+          dataIndex: 'ElectionDate',
+          key: 'electionDate',
+          align: 'right',
+          sorter: (a, b) => sorterStringDate(a.ElectionDate, b.ElectionDate)
+        }
+      ]
+    },
+    {
+      title: 'מאפייני התורם/ המלווה/ הערב',
+      key: 'entityCharacteristics',
+      align: 'center',
+      children: [
+        {
+          title: 'שם מלא',
+          dataIndex: 'FullName',
+          key: 'fullName',
+          align: 'right',
+          sorter: (a, b) => sorterString(a.FullName, b.FullName)
+        },
+        {
+          title: 'ארץ',
+          dataIndex: 'Country',
+          key: 'country',
+          align: 'right',
+          sorter: (a, b) => sorterString(a.Country, b.Country)
+        },
+        {
+          title: 'ישוב',
+          dataIndex: 'EntityCity',
+          key: 'entityCity',
+          align: 'right',
+          sorter: (a, b) => sorterString(a.EntityCity, b.EntityCity)
+        }
+      ]
+    },
+    {
+      title: 'מאפייני התרומה/ ההלוואה/ הערבות',
+      key: 'fundingCharacteristics',
+      align: 'center',
+      children: [
+        {
+          title: 'תאריך',
+          dataIndex: 'FundingDate',
+          key: 'fundingDate',
+          align: 'right',
+          sorter: (a, b) => sorterStringDate(a.FundingDate, b.FundingDate)
+        },
+        {
+          title: 'סכום',
+          dataIndex: 'FundingAmount',
+          key: 'fundingAmount',
+          align: 'right',
+          sorter: (a, b) => a.FundingAmount - b.FundingAmount,
+          render: amount =>
+            amount.toLocaleString('he-IL', {
+              style: 'decimal',
+              minimumFractionDigits: 2
+            })
+        },
+        {
+          title: 'יתרת הלוואה',
+          dataIndex: 'LoanBalance',
+          key: 'loanBalance',
+          align: 'right',
+          sorter: (a, b) => (a.LoanBalance ?? 0) - (b.LoanBalance ?? 0),
+          render: balance =>
+            balance &&
+            balance.toLocaleString('he-IL', {
+              style: 'decimal',
+              minimumFractionDigits: 2
+            })
+        }
+      ]
+    }
+  ];
+
+  const tableProps: TableProps<PublishResult> = {
+    data: data.map(d => ({ ...d, key: d.ID })),
+    columns,
     loading: false,
     pagination: {
       current: 1,
       pageSize: 10,
       total: data.length,
       showSizeChanger: true,
-      pageSizeOptions: ['10', '50', '100', '200']
+      pageSizeOptions: ['10', '25', '50', '100']
     },
     scroll: { x: 'max-content', y: 510 },
     rowKey: 'key',
--- app/utils/utils.ts
+++ app/utils/utils.ts
@@ -5,3 +5,11 @@ export const toDayjs = (value: DatePickerType): Dayjs | undefined => {
   if (!value) return undefined;
   return dayjs.isDayjs(value) ? value : dayjs(value, 'DD/MM/YYYY');
 };
+
+export const parseDate = (dateString: string): number => {
+  const [day, month, year] = dateString.split('/').map(Number);
+  return new Date(year, month - 1, day).getTime();
+};
+
+export const sorterString = (a: string, b: string) => a.localeCompare(b);
+export const sorterStringDate = (a: string, b: string) => parseDate(a) - parseDate(b);
