4439664edbcbd173a896628be2e1900f8cf44663
chaya_b <chaya_b@malam.com>
2025-04-21 14:03:42 +0300

    ComponentOverviewPage

--- app/routes.ts
+++ app/routes.ts
@@ -1,6 +1,7 @@
 import { type RouteConfig, route } from '@react-router/dev/routes';
 
 export default [
+  route('component-overview', 'routes/component-overview.tsx'),
   route('publisher', 'routes/publisher.tsx'),
   route('registration', 'routes/registration.tsx')
 ] satisfies RouteConfig;
---
+++ app/routes/component-overview.tsx
@@ -0,0 +1,7 @@
+import ComponentOverviewPage from '~/pages/component-overview';
+
+export const ComponentOverview = () => {
+  return <ComponentOverviewPage />;
+};
+
+export default ComponentOverview;
