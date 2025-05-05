e686ce4bd7e45d45932d06826245f496bcb5a4c3
Tzivi Doitch <tzivi_d@malam.com>
2025-05-05 18:24:35 +0300

    delete unccessary file

--- app/components/publisher/summary-result/summary-results-data.ts
+++
@@ -1,9 +0,0 @@
-import DonationIcon from '~/assets/images/DonationIcon.svg';
-import { DividerType, type SummaryResultData } from '~/types/publish-summary-result';
-
-export const stats: SummaryResultData[] = [
-  { title: 'רשומות', count: 45, sum: 657416.20, iconSrc: DonationIcon, dividerAfter: DividerType.Line },
-  { title: 'הלוואות', count: 9, sum: 53000.00, iconSrc: DonationIcon, dividerAfter: DividerType.Line },
-  { title: 'ערבויות', count: 1, sum: 10000.00, iconSrc: DonationIcon, dividerAfter: DividerType.Arrow },
-  { title: 'תרומות', count: 35, sum: 112416.20, iconSrc: DonationIcon },
-];
