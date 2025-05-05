843577edc158c84d77d57ca89c2f6ade8052bf65
chaya_b <chaya_b@malam.com>
2025-05-05 16:43:05 +0300

    InputWrapper - design

--- shared/components/form/check-box/check-box.tsx
+++ shared/components/form/check-box/check-box.tsx
@@ -1,12 +1,17 @@
 import InputWrapper from '../input-wrapper/input-wrapper';
 import type { FormFieldProps } from '@app-types/form-types';
 
-export interface CheckBoxProps extends FormFieldProps<boolean> {}
+export type CheckBoxProps = FormFieldProps<boolean>;
 
 export const CheckBox = ({ id, label, value, onChange, error }: CheckBoxProps) => {
   return (
-    <InputWrapper id={id} error={error} label={label} value={null} hasFloatingLabel={false}>
-      <input id={id} type="checkbox" checked={value} onChange={e => onChange?.(e.target.checked)} />
+    <InputWrapper id={id} error={error} label={label}>
+      <input
+        id={id}
+        type="checkbox"
+        checked={!!value}
+        onChange={e => onChange?.(e.target.checked)}
+      />
     </InputWrapper>
   );
 };
