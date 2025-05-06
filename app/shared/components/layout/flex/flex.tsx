cb61a5d2ca35521ad27bb0d94f8e6ec5f1432adb
chaya_b <chaya_b@malam.com>
2025-05-06 10:36:25 +0300

    Flex width

--- app/shared/components/layout/flex/flex.tsx
+++ app/shared/components/layout/flex/flex.tsx
@@ -4,12 +4,16 @@ import styles from './flex.module.css';
 export interface FlexProps {
   direction?: 'row' | 'column';
   children: ReactNode;
+  width?: string | number;
 }
 
-export const Flex = ({ children, direction = 'row' }: FlexProps) => {
+export const Flex = ({ children, direction = 'row', width = '' }: FlexProps) => {
   return (
     <div
       className={`${styles.flex} ${direction === 'column' ? styles['flex-direction-column'] : ''}`}
+      style={
+        { '--flex-width': typeof width === 'number' ? `${width}px` : width } as React.CSSProperties
+      }
     >
       {children}
     </div>
