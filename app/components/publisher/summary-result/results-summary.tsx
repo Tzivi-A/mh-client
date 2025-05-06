1b9546d991d5edab63436765ee3f73ccc85a8c32
Tzivi Doitch <tzivi_d@malam.com>
2025-05-06 14:16:33 +0300

    arrange the code by lint --fix

--- app/components/publisher/summary-result/results-summary.tsx
+++ app/components/publisher/summary-result/results-summary.tsx
@@ -4,26 +4,26 @@ import { DividedRowList } from '@ui/divided-row-list/divided-row-list';
 import { currencyFormatter } from '@utils/number-utils';
 
 interface PublisherResultsSummaryProps {
-    items: PublishResultSummaryData[];
+  items: PublishResultSummaryData[];
 }
 
 export const PublisherResultSummary = ({ items }: PublisherResultsSummaryProps) => {
-    return (
-        <DividedRowList
-            items={items}
-            renderItem={({ title, count, sum, iconSrc }) => (
-                <div className="results-summary-item">
-                    <div className="results-summary-info">
-                        <div className="results-summary-title">
-                            {count ? `${count} ${title}` : `0 ${title}`}
-                        </div>
-                        <div className="results-summary-sum">{currencyFormatter.format(sum)}</div>
-                    </div>
-                    <div className="results-summary-icon">
-                        <img src={iconSrc} alt={`${title} icon`} />
-                    </div>
-                </div>
-            )}
-        />
-    );
+  return (
+    <DividedRowList
+      items={items}
+      renderItem={({ title, count, sum, iconSrc }) => (
+        <div className="results-summary-item">
+          <div className="results-summary-info">
+            <div className="results-summary-title">
+              {count ? `${count} ${title}` : `0 ${title}`}
+            </div>
+            <div className="results-summary-sum">{currencyFormatter.format(sum)}</div>
+          </div>
+          <div className="results-summary-icon">
+            <img src={iconSrc} alt={`${title} icon`} />
+          </div>
+        </div>
+      )}
+    />
+  );
 };
--- app/mappers/publisher/funding-type-mapper.ts
+++ app/mappers/publisher/funding-type-mapper.ts
@@ -5,40 +5,42 @@ import type { LocalGuarantyDonationSearch } from '~/types/publisher/publisher-se
 import { FundingTypeIcons, FundingTypeTitles } from '~/utils/consts/publisher/funding-type';
 import SummaryIcon from '~/assets/images/publisher/summary.svg';
 
-export const mapperSummaryData = (result: LocalGuarantyDonationSearch): PublisherResultSummaryData[] => {
-    const summaryData: PublisherResultSummaryData[] = result
-        ? [
-            {
-                fundingType: FundingTypeEnum.Donation,
-                title: FundingTypeTitles[FundingTypeEnum.Donation].plural,
-                count: result.numDonations,
-                sum: parseFloat(result.sumDonations),
-                iconSrc: FundingTypeIcons[FundingTypeEnum.Donation],
-                dividerAfter: DividerTypeEnum.Line
-            },
-            {
-                fundingType: FundingTypeEnum.Guarantee,
-                title: FundingTypeTitles[FundingTypeEnum.Guarantee].plural,
-                count: result.numGuarantees,
-                sum: parseFloat(result.sumGuarantees),
-                iconSrc: FundingTypeIcons[FundingTypeEnum.Guarantee],
-                dividerAfter: DividerTypeEnum.Line
-            },
-            {
-                fundingType: FundingTypeEnum.Loan,
-                title: FundingTypeTitles[FundingTypeEnum.Loan].plural,
-                count: result.numLoans,
-                sum: parseFloat(result.sumLoans),
-                iconSrc: FundingTypeIcons[FundingTypeEnum.Loan],
-                dividerAfter: DividerTypeEnum.Arrow
-            },
-            {
-                title: 'רשומות',
-                count: result.numLoans + result.numGuarantees + result.numDonations,
-                sum: parseFloat(result.sumLoans + result.sumGuarantees + result.sumDonations),
-                iconSrc: SummaryIcon
-            }
-        ]
-        : [];
-    return summaryData;
+export const mapperSummaryData = (
+  result: LocalGuarantyDonationSearch
+): PublisherResultSummaryData[] => {
+  const summaryData: PublisherResultSummaryData[] = result
+    ? [
+        {
+          fundingType: FundingTypeEnum.Donation,
+          title: FundingTypeTitles[FundingTypeEnum.Donation].plural,
+          count: result.numDonations,
+          sum: parseFloat(result.sumDonations),
+          iconSrc: FundingTypeIcons[FundingTypeEnum.Donation],
+          dividerAfter: DividerTypeEnum.Line
+        },
+        {
+          fundingType: FundingTypeEnum.Guarantee,
+          title: FundingTypeTitles[FundingTypeEnum.Guarantee].plural,
+          count: result.numGuarantees,
+          sum: parseFloat(result.sumGuarantees),
+          iconSrc: FundingTypeIcons[FundingTypeEnum.Guarantee],
+          dividerAfter: DividerTypeEnum.Line
+        },
+        {
+          fundingType: FundingTypeEnum.Loan,
+          title: FundingTypeTitles[FundingTypeEnum.Loan].plural,
+          count: result.numLoans,
+          sum: parseFloat(result.sumLoans),
+          iconSrc: FundingTypeIcons[FundingTypeEnum.Loan],
+          dividerAfter: DividerTypeEnum.Arrow
+        },
+        {
+          title: 'רשומות',
+          count: result.numLoans + result.numGuarantees + result.numDonations,
+          sum: parseFloat(result.sumLoans + result.sumGuarantees + result.sumDonations),
+          iconSrc: SummaryIcon
+        }
+      ]
+    : [];
+  return summaryData;
 };
