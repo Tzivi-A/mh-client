907e942fdc61ae788e44c9433779c6943a7e7c7b
Tzivi Doitch <tzivi_d@malam.com>
2025-05-06 08:51:15 +0300

    remove somethings

--- app/pages/publisher/index.tsx
+++ app/pages/publisher/index.tsx
@@ -3,9 +3,6 @@ import PublisherBanner from '../../components/publisher/banner/publisher-banner'
 import type { SummaryResultData } from "~/types/publish-summary-result";
 import DonationIcon from '~/assets/images/DonationIcon.svg';
 import { DividerType } from '@app-types/enums/divider-type';
-import { PublishResultTable } from '~/components/publisher/table-result/table-result';
-import { useAppQuery } from '~/hooks/api/queries/use-app-query';
-import type { PublishResult } from '~/types/publish-result';
 
 export const summaryData: SummaryResultData[] = [
   { title: 'רשומות', count: 45, sum: 657416.20, iconSrc: DonationIcon, dividerAfter: DividerType.Line },
@@ -14,11 +11,7 @@ export const summaryData: SummaryResultData[] = [
   { title: 'תרומות', count: 35, sum: 112416.24, iconSrc: DonationIcon },
 ];
 
-
 export const PublisherPage = () => {
-  const publishResult = useAppQuery<PublishResult[]>({
-    url: 'api/publisher/localGuarantyDonationSearch'
-  });
   return (
     <div>
       <h1>תרומות, ערבויות והלוואות לסיעות בבחירות לרשויות מקומיות</h1>
@@ -26,7 +19,6 @@ export const PublisherPage = () => {
       <h3>חיפוש תרומה/ ערבות/ הלוואה</h3>
       <PublisherBanner />
       <SummaryResults items={summaryData} />
-      <PublishResultTable publishResult={publishResult} loading={!publishResult} />
     </div>
   );
 };
