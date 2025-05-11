87a25629f773f70897c7b50ca5355c3d2150547d
chaya_b <chaya_b@malam.com>
2025-05-11 17:22:31 +0300

    delete pareseNumber

--- app/mappers/publisher/publication-search-mapper.ts
+++ app/mappers/publisher/publication-search-mapper.ts
@@ -3,7 +3,7 @@ import { PublicationSearchEnum } from '~/types/enums/publication-search';
 import type { PublisherResultSummaryData } from '~/types/publisher/publisher-summary-result-type';
 import type { LocalPublicationResults } from '~/types/publisher/publisher-search-results-type';
 import { PublicationSearchTitles } from '~/utils/constants/publisher/publication-search';
-import { formatHebrewNumber, pareseNumber } from '@utils/number-utils';
+import { formatHebrewNumber } from '@utils/number-utils';
 
 export const mapperSummaryData = (
   result?: LocalPublicationResults
