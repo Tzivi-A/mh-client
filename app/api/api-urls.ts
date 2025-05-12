0a756e131f2b6112a50e8135270f6b1eb7775953
chaya_b <chaya_b@malam.com>
2025-05-12 11:51:40 +0300

    GET_CODE_ENTITY_LIST

--- app/api/api-urls.ts
+++ app/api/api-urls.ts
@@ -4,9 +4,12 @@ const FACTION_API = 'api/faction';
 const PUBLISHER_API = 'api/publisher';
 
 export const GET_ACTIVE_LOCAL_ELECTIONS = `${ELECTION_API}/activeLocalElections`;
+
 export const GET_COUNTRIES = `${CODE_TABLE_API}/countries`;
 export const GET_CITIES = `${CODE_TABLE_API}/cities`;
-export const GET_PUBLICATION = `${CODE_TABLE_API}/publicationSearch`;
+export const GET_CODE_ENTITY_LIST = `${CODE_TABLE_API}/codeTable`;
+
 export const GET_FACTION_CITIES = `${FACTION_API}/cities`;
 export const GET_FACTIONS = `${FACTION_API}/factions`;
+
 export const LOCAL_PUBLICATION_SEARCH = `${PUBLISHER_API}/localPublicationSearch`;
