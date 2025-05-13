30ff72fdbbc61d40462ee5329d253bb948cfbfc5
chaya_b <chaya_b@malam.com>
2025-05-13 10:19:42 +0300

    mh-header

--- app/components/mh-header/mh-header.tsx
+++ app/components/mh-header/mh-header.tsx
@@ -2,21 +2,41 @@ import Header from '@ui/header/header';
 import { Image } from '@ui/image/image';
 import { Divider } from '@ui/divided-row-list/divider';
 import { Flex } from '@ui/layout/flex/flex';
+import Link from '@ui/link/link';
 import { DividerTypeEnum } from '@app-types/enums/divider-type';
 import LogoHeader from '~/assets/images/logo_mevaker_hebrew.svg';
-import './mh-header.css';
+import LinkIcon from '~/assets/images/external-link.svg';
+import styles from './mh-header.module.css';
 
 export interface HeaderProps {
-  children?: React.ReactNode;
+  title?: string;
+  isLink?: boolean;
 }
 
-export const MHHeader = ({ children }: HeaderProps) => {
+export const MHHeader = ({ title, isLink = false }: HeaderProps) => {
   return (
-    <Header className="mh-header">
+    <Header className={styles['mh-header']}>
       <Flex align="center">
         <Image src={LogoHeader} alt="logo" />
         <Divider type={DividerTypeEnum.Line} />
-        <div className="children-header">{children}</div>
+        <div className={styles['children-header']}>
+          <Flex justify="space-between">
+            <h3>{title}</h3>
+            {isLink && (
+              <div className={styles['mh-link']}>
+                <Link
+                  href="https://www.mevaker.gov.il/"
+                  target="_blank"
+                  className={styles['link-icon']}
+                  rel="noreferrer"
+                >
+                  <Image src={LinkIcon} alt="Mevaker hamedina link" />
+                  לאתר מבקר המדינה
+                </Link>
+              </div>
+            )}
+          </Flex>
+        </div>
       </Flex>
     </Header>
   );
--- app/pages/publisher/index.tsx
+++ app/pages/publisher/index.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import PublisherBanner from '~/components/publisher/banner/banner';
 import PublisherResult from '~/components/publisher/publisher-result/publisher-result';
-import { PublisherHeader } from '~/components/publisher/header/header';
+import MHHeader from '~/components/mh-header/mh-header';
 import type { LocalPublicationResults } from '~/types/publisher/publisher-search-results-type';
 
 export const PublisherPage = () => {
@@ -9,7 +9,7 @@ export const PublisherPage = () => {
 
   return (
     <div>
-      <PublisherHeader title={'סייעות בבחירות לרשויות מקומיות'} />
+      <MHHeader title={'תרומות, ערבויות והלוואות לסיעות בבחירות לרשויות מקומיות'} isLink={true} />
       <h1>חיפוש תרומה/ ערבות/ הלוואה</h1>
       <span>המידע על התרומות מפורסם בהסתמך על הדיווח של המועמד בלבד ועל אחריותו.</span>
       <PublisherBanner setSearchData={setSearchData} />
