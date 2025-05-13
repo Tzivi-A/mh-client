bd969157d616b095f4dc19ed0a98e943066e53d5
Tzivi Doitch <tzivi_d@malam.com>
2025-05-13 13:12:56 +0300

    add filter by publication type and add no result comp

--- app/components/publisher/publisher-result/publisher-result.tsx
+++ app/components/publisher/publisher-result/publisher-result.tsx
@@ -1,3 +1,4 @@
+import { useMemo, useState } from 'react';
 import { PublisherResultSummary } from '~/components/publisher/results-summary/results-summary';
 import PublisherResultTable from '~/components/publisher/result-table/result-table';
 import type { PublisherResultSummaryData } from '~/types/publisher/publisher-summary-result-type';
@@ -8,6 +9,9 @@ import type {
   LocalPublicationResults
 } from '~/types/publisher/publisher-search-results-type';
 import Section from '@ui/section/section';
+import type { Option } from '@app-types/option-type';
+import { CheckBoxGroup } from '@ui/form/check-box/check-box-group/check-box-group';
+import NoResults from '@ui/no-results/no-results';
 
 export interface PublisherResultProps {
   data?: LocalPublicationResults;
@@ -16,6 +20,37 @@ export interface PublisherResultProps {
 export const PublisherResult = ({ data }: PublisherResultProps) => {
   const tableData: LocalPublicationResultRow[] = data?.results ?? [];
   const summaryData: PublisherResultSummaryData[] = mapperSummaryData(data);
+
+  // Generate publication-type-filter options once
+  const publicationTypeOptions: Option[] = useMemo(() => {
+    const uniqueOptions = new Map<number, Option>();
+
+    tableData.forEach(row => {
+      const { id, name } = row.publicationType ?? {};
+      if (id !== null && !uniqueOptions.has(id)) {
+        uniqueOptions.set(id, { value: id, label: name });
+      }
+    });
+
+    return Array.from(uniqueOptions.values());
+  }, [tableData]);
+
+  // Init all types selected by default
+  const [publicationTypesFilter, setPublicationTypesFilter] = useState(() =>
+    publicationTypeOptions.map(opt => opt.value)
+  );
+
+  // Filter data based on selected types
+  const filteredData = useMemo(() => {
+    if (publicationTypesFilter.length === 0) {
+      return [];
+    }
+    return tableData.filter(row => {
+      const id = row.publicationType?.id;
+      return id !== null && publicationTypesFilter.includes(id);
+    });
+  }, [tableData, publicationTypesFilter]);
+
   const localElectionColumns: ColumnsType<LocalPublicationResultRow> = [
     {
       title: 'סיעה',
@@ -41,17 +76,23 @@ export const PublisherResult = ({ data }: PublisherResultProps) => {
     }
   ];
 
+  if (data?.results?.length === 0) {
+    return <NoResults />;
+  }
+
   return (
     <div>
       <PublisherResultSummary items={summaryData} />
-      <Section
-        header={
-          'שימו לב: ניתן להציג עד 1000 רשומות. מוצגות בטבלה 1000 האחרונות, נא לצמצם את נתוני החיפוש.'
-        }
-        children={
-          <PublisherResultTable data={tableData} electionColumnsChildren={localElectionColumns} />
-        }
+      <CheckBoxGroup
+        id="publication-type-filter"
+        options={publicationTypeOptions}
+        value={publicationTypesFilter}
+        onChange={setPublicationTypesFilter}
+        flexDirection="row"
       />
+      <Section header="שימו לב: ניתן להציג עד 1000 רשומות. מוצגות בטבלה 1000 האחרונות, נא לצמצם את נתוני החיפוש.">
+        <PublisherResultTable data={filteredData} electionColumnsChildren={localElectionColumns} />
+      </Section>
     </div>
   );
 };
