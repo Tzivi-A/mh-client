191e8fef0bad4b8319b163cc68d45d671f4b5fcc
Tzivi-A <tzivi_d@malam.com>
2025-05-14 16:44:42 +0300

    Create StepItem component

---
+++ app/shared/components/step-item/step-item.tsx
@@ -0,0 +1,23 @@
+import { Image } from '@ui/image/image';
+import styles from './step-item.module.css';
+
+export interface StepItemProps {
+  icon: string;
+  title: string;
+  subtitle: string;
+  isActive?: boolean;
+}
+
+export const StepItem = ({ icon, title, subtitle, isActive = false }: StepItemProps) => {
+  return (
+    <div className={`${styles.step} ${isActive ? styles.active : ''}`}>
+      <Image src={icon} alt={subtitle} className={styles['step-icon']} />
+      <div className={styles['step-text-container']}>
+        <span className={styles['step-title']}>{title}</span>
+        <span className={styles['step-subtitle']}>{subtitle}</span>
+      </div>
+    </div>
+  );
+};
+
+export default StepItem;
