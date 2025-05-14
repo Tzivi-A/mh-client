96fbb626e543ba370f147a05f3bc4b3a82e77a52
Tzivi-A <tzivi_d@malam.com>
2025-05-14 14:43:10 +0300

    Update Switch to use Ant Design

--- app/shared/components/form/switch/switch.tsx
+++ app/shared/components/form/switch/switch.tsx
@@ -1,6 +1,6 @@
+import { Switch as AntSwitch } from 'antd';
 import InputWrapper from '@ui/form/input-wrapper/input-wrapper';
 import type { FormFieldProps } from '@app-types/form-type';
-import styles from './switch.module.css';
 
 export interface SwitchProps extends FormFieldProps<boolean> {
   label?: string;
@@ -9,20 +9,11 @@ export interface SwitchProps extends FormFieldProps<boolean> {
 export const Switch = ({ id, label, value, onChange, error, isRequired }: SwitchProps) => {
   return (
     <InputWrapper id={id} label={label} error={error} isRequired={isRequired}>
-      <div className={styles['switch-container']}>
-        <input
-          id={`${id}-input`}
-          type="checkbox"
-          checked={value}
-          onChange={e => onChange?.(e.target.checked)}
-          className={styles.input}
-          aria-label={label}
-        />
-        <label htmlFor={`${id}-input`} className={styles.switch}>
-          <span className={styles.slider} />
-          <span className={styles['sr-only']}>{label}</span>
-        </label>
-      </div>
+      <AntSwitch
+        id={id}
+        checked={value}
+        onChange={checked => onChange?.(checked)}
+      />
     </InputWrapper>
   );
 };
