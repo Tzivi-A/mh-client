b55cf468b38e1dbf023535167261e1eebcccc85d
Tzivi-A <tzivi_d@malam.com>
2025-05-14 12:26:44 +0300

    Create registration mapper and update registration progress component

--- app/components/registration/registration-progress/registration-progress.tsx
+++ app/components/registration/registration-progress/registration-progress.tsx
@@ -2,10 +2,7 @@ import { Image } from '@ui/image/image';
 import { DividedRowList } from '@ui/divided-row-list/divided-row-list';
 import { DividerTypeEnum } from '@app-types/enums/divider-type';
 import styles from './registration-progress.module.css';
-import PersonalIcon from '~/assets/images/registration/personal-details.svg';
-import PartyIcon from '~/assets/images/registration/party-details.svg';
-import BankIcon from '~/assets/images/registration/bank-details.svg';
-import ApprovalIcon from '~/assets/images/registration/approval.svg';
+import { registrationStepsMapper } from '~/mappers/registration/registration-progress-mapper';
 
 export interface RegistrationStep {
   id: number;
@@ -13,32 +10,6 @@ export interface RegistrationStep {
   icon: string;
   dividerAfter?: DividerTypeEnum;
 }
-
-const steps: RegistrationStep[] = [
-  {
-    id: 1,
-    title: 'פרטי המועמד והתמודדות',
-    icon: PersonalIcon,
-    dividerAfter: DividerTypeEnum.Arrow
-  },
-  {
-    id: 2,
-    title: 'פרטי הסיעה והבנק',
-    icon: PartyIcon,
-    dividerAfter: DividerTypeEnum.Arrow
-  },
-  {
-    id: 3,
-    title: 'פרטי איש קשר',
-    icon: BankIcon,
-    dividerAfter: DividerTypeEnum.Arrow
-  },
-  {
-    id: 4,
-    title: 'אישור פרטים',
-    icon: ApprovalIcon
-  }
-];
 
 interface RegistrationProgressProps {
   currentStep: number;
@@ -55,7 +26,7 @@ export const RegistrationProgress = ({ currentStep }: RegistrationProgressProps)
   return (
     <div className={styles['progress-container']}>
       <DividedRowList
-        items={steps}
+        items={registrationStepsMapper}
         renderItem={step => (
           <div className={`${styles.step} ${getStepClassName(step.id)}`}>
             <Image src={step.icon} alt={step.title} className={styles['step-icon']} />
