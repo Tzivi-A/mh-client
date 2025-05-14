f692d32b9a763c706f615932b10a1f73a8d72081
Tzivi-A <tzivi_d@malam.com>
2025-05-14 15:43:49 +0300

    Create stepper components and update registration

--- app/pages/registration/index.tsx
+++ app/pages/registration/index.tsx
@@ -3,16 +3,40 @@ import { RegistrationProgress } from '~/components/registration/registration-pro
 import MHHeader from '~/components/mh-header/mh-header';
 import Section from '@ui/section/section';
 import RegistrationForm from '~/components/registration/registration-form/registration-form';
+import { Instructions, type Step } from '@ui/instructions/instructions';
 
 export const RegistrationPage = () => {
   const [currentStep] = useState(2);
+
+  const steps: Step[] = [
+    {
+      id: 1,
+      title: 'פרטי המועמד והתמודדות',
+      component: <RegistrationForm />
+    },
+    {
+      id: 2,
+      title: 'פרטי הסיעה והבנק',
+      component: <div>Step 2 Content</div>
+    },
+    {
+      id: 3,
+      title: 'פרטי איש קשר',
+      component: <div>Step 3 Content</div>
+    },
+    {
+      id: 4,
+      title: 'אישור פרטים',
+      component: <div>Step 4 Content</div>
+    }
+  ];
 
   return (
     <div>
       <MHHeader title="מערכת לניהול חשבונות" isLink={false} />
       <Section header="טופס פניית מועמד לתחילת דיווח">
         <RegistrationProgress currentStep={currentStep} />
-        <RegistrationForm />
+        <Instructions steps={steps} initialStep={currentStep} />
       </Section>
     </div>
   );
