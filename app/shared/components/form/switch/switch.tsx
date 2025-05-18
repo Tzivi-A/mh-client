18afca57ef6833355b7e4217cbd86af4731993fb
Tzivi-A <tzivi_d@malam.com>
2025-05-18 11:43:22 +0300

    Fix Switch component to use input element

--- app/shared/components/form/switch/switch.tsx
+++ app/shared/components/form/switch/switch.tsx
@@ -1,7 +1,6 @@
 import type { FormFieldProps } from '@app-types/form-type';
 import styles from './switch.module.css';
 import { Flex } from '@ui/layout/flex/flex';
-import { CheckBox } from '../check-box/check-box';
 
 export interface SwitchProps extends FormFieldProps<boolean> {
   label?: string;
@@ -11,10 +10,11 @@ export const Switch = ({ id, label, value, onChange }: SwitchProps) => {
   return (
     <Flex direction="column">
       <label className={styles['switch-container']}>
-        <CheckBox
+        <input
+          type="checkbox"
           id={id}
-          value={value}
-          onChange={onChange}
+          checked={value}
+          onChange={e => onChange?.(e.target.checked)}
           className={styles.input}
           aria-label={label}
         />
